1fbbe59818e7e9812f41bed541305c3b351fff17
wuyaheng <wuyaheng2016@gmail.com>
2021-01-09 20:10:17 -0500

    remove line chart

--- src/components/ResultList/index.js
+++ src/components/ResultList/index.js
@@ -87,67 +87,6 @@ function ResultList(props) {
   };
 
 
-
-//   const LineChart = ({ type }) => {
-//     const obj = {};
-
-//     let options = {
-//       legend: {
-//         display: false,
-//       },
-//       title: {
-//         display: true,
-//         text: 'Number of Arrests Over Time'
-//      },
-//       scales: {
-//         xAxes: [
-//           {
-//             gridLines: {
-//               display: false,
-//             }
-//           }
-//         ],
-//         yAxes: [{
-//           ticks: {
-//               beginAtZero: true,
-//               min: 0,
-//               stepSize: 1
-//           },
-//           stacked: true
-//       }]
-//       }
-//     };
-
-//     props.results.forEach((arrest) => {
-//       const key = arrest[type].substring(0, 10);
-//       if (key)
-//         if (obj[key]) {
-//           obj[key] += 1;
-//         } else {
-//           obj[key] = 1;
-//         }
-//     });
-
-//     let entries =
-//       Object.entries(obj).sort((a, b) => (a[0] > b[0] ? 1 : -1)) || [];
-//      return (
-//         <Line
-//           data={{
-//             labels: entries.map((x) => x[0]),
-//             datasets: [
-//               {
-//                 data: entries.map((x) => x[1]),
-//                 backgroundColor: "#a4c3b2", 
-//               },
-//             ],
-//           }} 
-//           options={options} 
-//         />
-//       );
-
-//   };
-
-
   return (
     <div className="container-fluid mb-2">
       <div className="row">
@@ -156,15 +95,7 @@ function ResultList(props) {
           <PieChart type="critical_flag" />
           </div>
         </div>{" "}
-
       </div>{" "}
-      {/* <div className="row mt-2">
-      <div className="col-md-12" style={{paddingLeft: "2px", paddingRight: "2px"}}>
-      <div className="card" style={{paddingBottom: "2px"}}>
-        <LineChart type="arrest_date"/> 
-      </div>
-      </div>
-      </div> */}
     </div>
   );
 }
